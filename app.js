d2b72bbc893575d3745cf6b6eb62407618bb1e6e
Adrian Daniel Abadin <aabadin@gmail.com>
2022-11-12 07:36:32 -0300

    Agredado Multer

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const routes = require("./routes/products");
 const app = express();
-app.listen(8080, () => console.log("Server Up on port 8080"));
 app.use(express.static("public"));
 app.use(express.json());
+app.use("/", routes);
 app.engine("handlebars", handlebars.engine());
 app.set("views", "./views");
 app.set("view engine", "handlebars");
 app.get("/", (req, res) => res.render("home"));
+app.listen(8080, () => console.log("Server Up on port 8080"));
--- routes/products.js
+++ routes/products.js
@@ -1,2 +1,22 @@
 const express = require("express");
+const multer = require("multer");
 const routes = express.Router();
+const Store = require("../funciones/storage");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "" + Date.now() + "-" + file.originalname);
+  },
+});
+let upload = multer({ storage: storage });
+const dbManager = new Store("./data.json");
+routes.use(express.json());
+routes.post("/addproduct", upload.single("url"), async (req, res) => {
+  const { title, url, price } = req.body;
+  const response = await dbManager.addProduct(title, url, price, this.version);
+  console.log(req.file);
+  res.send(response);
+});
+module.exports = routes;
